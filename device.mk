1780f1eca06b2e689e3656ce75f4152ca043e4bd
Smita Ghosh <smitag@codeaurora.org>
2020-03-17 16:16:12 -0700

    warm: Add vndservicemanager explicitly
    This is now added to product_packages based on shipping_api which is
    not yet supported on few targets. Hence adding it explicitly
    
    Change-Id: I5d3dfcbf333e296cea6f6e25c4e27c8fe85929e1

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Verified boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
