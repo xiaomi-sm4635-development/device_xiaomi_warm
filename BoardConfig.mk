e663916dd266e593430e2ba45d253cac4ab70dcf
Luofan Chen <clfbbn@gmail.com>
2024-02-02 20:49:44 +0800

    warm: Import QCC system_ext blobs
    Makes the QTI GNSS service happy. Thanks luca020400 for the help.
    
    Change-Id: I6de25b0b3fde6cce3a6cdf95f31c68734d429956

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,6 +199,8 @@ DEVICE_MANIFEST_SKUS := pitti
 DEVICE_MANIFEST_PITTI_FILES := \
     $(DEVICE_PATH)/configs/vintf/manifest_pitti.xml
 
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
