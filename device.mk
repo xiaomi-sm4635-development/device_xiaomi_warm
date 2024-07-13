cfb7587ac5219a7533a2ceba78c7f1342636c753
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    warm: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -112,6 +112,10 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     IPACM_Filter_cfg.xml
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.memtrack-service
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
