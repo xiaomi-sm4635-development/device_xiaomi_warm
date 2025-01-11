6099b93412b52ae1a323a9edd70a519e2089e644
LuK1337 <priv.luk@gmail.com>
2025-01-11 17:05:20 +0100

    warm: Create vendor mount points
    Change-Id: I13dc0d47145d1ca6dc1fdceb091fcc7c83cb8637

--- device.mk
+++ device.mk
@@ -103,6 +103,11 @@ PRODUCT_COPY_FILES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_pitti/android.hardware.sensor.accelerometer.xml \
