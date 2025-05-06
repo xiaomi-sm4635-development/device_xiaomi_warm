cb2e514de34b4bbd849b0dd9b01480bf99561d52
Arian <arian.kulmer@web.de>
2025-05-06 14:23:33 +0530

    warm: sepolicy: Import initial sepolicy for Xiaomi parts
    * It was missing before

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Vendor security patch
 VENDOR_SECURITY_PATCH := 2025-11-01
