e10c1aa638299805dd2e45476211a0b75a64b19a
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-21 13:53:54 +0530

    warm: Build bluetooth audio impl
    Change-Id: I5e12dd5af8bf741f8a1c3d60cca43413a64cebbf

--- device.mk
+++ device.mk
@@ -51,6 +51,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 # Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
