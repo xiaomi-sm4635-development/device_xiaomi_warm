c4528ca8c0b93faf5d00b4f7db12bbebcb27140a
Michael Bestas <mkbestas@lineageos.org>
2025-12-12 07:34:01 +0200

    warm: Rename vendor libtensorflowlite_c.so
    libtensorflowlite_c target was added in BP4A and it conflicts with the
    prebuilt one. We can't use the source built one since it lacks
    necessary symbols.
    
    Change-Id: Idd85d0705e8af2ef39799fb4c952ea0a95b60162

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,14 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
+    (
+        'vendor/lib64/libcapiv2uvvendor.so',
+        'vendor/lib64/liblistensoundmodel2vendor.so',
+        'vendor/lib64/libVoiceSdk.so',
+    ): blob_fixup().replace_needed(
+        'libtensorflowlite_c.so',
+        'libtensorflowlite_c_vendor.so',
+    ),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libbinder_shim.so')
         .add_needed('libhidlbase_shim.so'),
