b14812525b380d43ecb7cfd6981b8f1678792d0f
Niyush-04 <mauryapradip04@gmail.com>
2026-02-15 15:11:11 +0530

    warm: overlay: Import runtime resource overlays

--- device.mk
+++ device.mk
@@ -125,6 +125,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayWarm \
+    FrameworkResOverlayWarm \
+    SettingsOverlayWarm \
+    SystemUIOverlayWarm \
+    TelephonyOverlayWarm \
+    WifiOverlayWarm
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
