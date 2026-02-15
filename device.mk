3c018e97ad32e4559cd32e8e68d8c830263b9b25
Niyush-04 <mauryapradip04@gmail.com>
2026-02-15 16:15:26 +0530

    warm: Setup biometrics fingerprint services

--- device.mk
+++ device.mk
@@ -145,6 +145,9 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.xiaomi
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
@@ -193,6 +196,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.warm.rc \
     ueventd.qcom.rc
 
 # IPACM
